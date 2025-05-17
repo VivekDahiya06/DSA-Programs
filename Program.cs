f428c39a1233c40aec202611e9b296d60f2a8898
VivekDahiya06 <vivekdahiya312@gmail.com>
2025-05-17 22:46:58 +0530

    Added all Folder to C# Branch.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿
+
+using DSA_Programs.Searching;
+using DSA_Programs.Sorting;
+
+namespace DSA_Programs
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Choose a program to run:");
+            Console.WriteLine("1. Linear Search");
+            Console.Write("Enter choice: ");
+            int choice = int.Parse(Console.ReadLine());
+            switch (choice) {
+                case 1: 
+                    Console.WriteLine("Launching Linear Search...\n");
+                    Linear_Search.Run(); 
+                    break;
+            }
+        }
+    }
+}
---
+++ Searching/Linear_Search.cs
@@ -0,0 +1,39 @@
+﻿
+namespace DSA_Programs.Searching
+{
+    public class Linear_Search
+    {
+
+        public static void linearSearch(int[] array, int target)
+        {
+            int size = array.Length;
+            for (int i = 0; i < size; i++)
+            {
+                if (array[i] == target)
+                {
+                    Console.WriteLine($"Element Found at index : {i}");
+                    break;
+                }
+            }
+        }
+
+        public static void Run()
+        {
+            Console.Write("Enter the size of Array : ");
+            int size = int.Parse(Console.ReadLine());
+            int[] array = new int[size];
+
+            Console.WriteLine("Enter the elements in Array :");
+            for (int i = 0; i < size; i++)
+            {
+                array[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine();
+            Console.Write("Enter the target element : ");
+            int target = int.Parse(Console.ReadLine());
+
+            linearSearch(array, target);
+        }
+    }
+}
