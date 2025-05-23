988f9de853ba4e1fb5b15c33ec7d08412bdce37e
VivekDahiya06 <vivekdahiya312@gmail.com>
2025-05-23 22:14:57 +0530

    Added Linked_List to Data_Structures.

--- Data_Structures/Linked_List/Linked_List.java
+++ Data_Structures/Linked_List/Linked_List.java
@@ -1,7 +1,75 @@
 package DSA_Programs.Data_Structures.Linked_List;
 
-public class Linked_List {
-    public static void main(String[] args) {
 
+class Node<T> {
+    T data;
+    Node<T> next;  // Self-Referential Field
+
+    Node(T data) {
+        this.data = data;
+        next = null;
+    }
+}
+
+public class Linked_List<T> {
+    Node<T> head = null;
+
+    public void add(T data) {
+        Node<T> newNode = new Node<>(data);
+        Node<T> currentNode = head;
+        if (head == null)
+            head = newNode;
+        else {
+            while (currentNode.next != null)
+                currentNode = currentNode.next;
+            currentNode.next = newNode;
+        }
+    }
+
+
+    public void addAtStart(T data){
+        Node<T> newNode = new Node<>(data);
+        newNode.next = head;
+        head = newNode;
+    }
+
+    public void delete(T data) {
+        Node<T> currentNode = head;
+
+        if(currentNode.data == data){
+            head = head.next;
+            return;
+        }
+        while (currentNode.next != null && currentNode.next.data != data ) {
+            currentNode = currentNode.next;
+        }
+        if(currentNode.next != null){
+            currentNode.next = currentNode.next.next;
+        }
+
+
+    }
+
+    public void printList() {
+        Node<T> currentNode = head;
+        System.out.print("[ ");
+        while (currentNode != null) {
+            System.out.print(currentNode.data + " ");
+            currentNode = currentNode.next;
+        }
+        System.out.println("]");
+    }
+
+    public static void main(String[] args) {
+        Linked_List list = new Linked_List();
+        list.add(1);
+        list.add(5);
+        list.add(6);
+        list.add(9);
+        System.out.println("Before Deletion : ");
+        list.printList();
+        list.delete(2);
+        System.out.println("After Deletion : ");
+        list.printList();
     }
 }
