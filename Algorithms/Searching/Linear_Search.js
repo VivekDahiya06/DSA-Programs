351038c10b63620f50c382154a3687ef99eb7bc5
VivekDahiya06 <vivekdahiya312@gmail.com>
2025-05-25 16:55:34 +0530

    Added Linear_Search to Algorithms/Searching.

--- Algorithms/Searching/Linear_Search.js
+++ Algorithms/Searching/Linear_Search.js
@@ -1,3 +1,17 @@
-const arr = [1,2,3,5];
+const array = [8, 4, 5, 2, 7, 9, 3, 1];
+const target = 9;
 
-console.log(arr);
+
+function Linear_Search(array, target) {
+    const size = array.length;
+        for (let i = 0; i < size; i++) {
+            if (array[i] === target) {
+                return i;
+            }
+        }
+        return -1;
+}
+
+const index = Linear_Search(array, target);
+if(index !== -1) console.log("Element Found at Index: ",index);
+else console.log("Element Not Found !!");
